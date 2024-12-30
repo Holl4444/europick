59470fefb29b892b3f3b824c7c932b99f8927515
Holly <162161116+Holl4444@users.noreply.github.com>
2024-12-30 23:56:11 +0000

    .

--- script.js
+++ script.js
@@ -3,15 +3,19 @@ const STAR = 12;
 const totBalls = 5;
 const totStars = 2;
 
-const numOptions = document.getElementById('num-options');
-const starOptions = document.getElementById('star-options');
-const numNumsChosen = document.getElementById('num-nums-picked');
-const numStarsChosen = document.getElementById('num-stars-picked');
-const confirmBtn = document.getElementById('confirm');
+const numOptions = document.getElementById("num-options");
+const starOptions = document.getElementById("star-options");
+const numNumsChosen = document.getElementById("num-nums-picked");
+const numStarsChosen = document.getElementById("num-stars-picked");
+const confirmBtn = document.getElementById("confirm");
 
 numNumsChosen.textContent = 0;
 numStarsChosen.textContent = 0;
 
+let choicesObject = {
+  nums: numChoices = [],
+  stars: starChoices = [],
+}
 
 const getRandom = (type) => Math.floor(Math.random() * type + 1);
 
@@ -51,71 +55,69 @@ const generateStars = () => {
 
 const generateDisplay = () => {
   const nums = generateBalls();
-    for (let i in nums) {
-        const ball = document.getElementById(`ball-${i}`);
-        ball.textContent = nums[i];
-    }
+  for (let i in nums) {
+    const ball = document.getElementById(`ball-${i}`);
+    ball.textContent = nums[i];
+  }
   const stars = generateStars();
-    for (let i in stars) {
-        const star = document.getElementById(`star-${i}`);
-         star.textContent = stars[i];
-    }
-    return nums.concat(stars);
+  for (let i in stars) {
+    const star = document.getElementById(`star-${i}`);
+    star.textContent = stars[i];
+  }
+  return nums.concat(stars);
 };
 
 const clear = () => {
-    for (i = 0; i < totBalls; i++) {
-        const ball = document.getElementById(`ball-${i}`);
-        ball.textContent = ""
-    }
-    for (i = 0; i < totStars; i++) {
-        const star = document.getElementById(`star-${i}`);
-        star.textContent = "";
-    }
-    return 1
-
+  for (i = 0; i < totBalls; i++) {
+    const ball = document.getElementById(`ball-${i}`);
+    ball.textContent = "";
+  }
+  for (i = 0; i < totStars; i++) {
+    const star = document.getElementById(`star-${i}`);
+    star.textContent = "";
+  }
+  return 1;
 };
-  
-
 
 //clear();
 
-
-
-let numChoices = [];
- numOptions.addEventListener("click", (e) => {
-    let txt = parseInt(numNumsChosen.textContent);   
-     if (!e.target.classList.contains("chosen")) {
-        if (txt < 5){
-            e.target.classList.toggle("chosen");
-            numNumsChosen.textContent = txt + 1;}
-            numChoices.push(text + 1);
-    } else if (txt > 0){
-        e.target.classList.toggle("chosen");
-        numNumsChosen.textContent = txt - 1;
-        // find index of txt in choices and remove it.
+numOptions.addEventListener("click", (e, choicesObject) => {
+  let txt = parseInt(numNumsChosen.textContent);
+  if (!e.target.classList.contains("chosen")) {
+    if (txt < 5) {
+      e.target.classList.toggle("chosen");
+      numNumsChosen.textContent = txt + 1;
+      choicesObject.nums.numChoices.push(txt);
     }
-    return 1;
- });
+    numChoices.push(text + 1);
+  } else if (txt > 0) {
+    e.target.classList.toggle("chosen");
+    numNumsChosen.textContent = txt - 1;
+    // find index of txt in choices and remove it.
+  }
+  console.table(choicesObject);
+  return 1;
+});
 
- let starChoices = []; // find how to access it
- starOptions.addEventListener("click", (e) => {
-    let txt = parseInt(numStarsChosen.textContent);   
-     if (!e.target.classList.contains("chosen")) {
-        if (txt < 2){
-            e.target.classList.toggle("chosen");
-            numStarsChosen.textContent = txt + 1;}
-    } else if (txt > 0){
-        e.target.classList.toggle("chosen");
-        numStarsChosen.textContent = txt - 1;
-        // find index of txt in choices and remove it.
+starOptions.addEventListener("click", (e, choicesObject) => {
+  let txt = parseInt(numStarsChosen.textContent);
+  if (!e.target.classList.contains("chosen")) {
+    if (txt < 2) {
+      e.target.classList.toggle("chosen");
+      numStarsChosen.textContent = txt + 1;
+      choicesObject.stars[0].push(txt);
+      console.table(choicesObject);
     }
-    return 1;
- });
+  } else if (txt > 0) {
+    e.target.classList.toggle("chosen");
+    numStarsChosen.textContent = txt - 1;
+    // find index of txt in choices and remove it. (.findIndex())
+  }
+  return 1;
+});
 
-  confirmBtn.addEventListener("click", () => {
-    const results = generateDisplay();
-    results.filter((i) =>  //userinputresults.contains[i]
-    )
-    //check
-})
+confirmBtn.addEventListener("click", () => {
+  const results = generateDisplay();
+  // results.filter((i) =>  userinputresults.contains[i])
+  //check
+});
